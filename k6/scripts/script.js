562d806b32e2a8c48fafc4f01b4fe56f2acc546f
Ismail Ktami <ismail.ktami@cdbdx.biz>
2022-03-22 01:37:15 +0100

    add docker compose config for selenium grid

--- k6/scripts/script.js
+++ k6/scripts/script.js
@@ -5,20 +5,27 @@ import { sleep,check } from 'k6';
 export let options = {
   stages: [
       // Ramp-up from 1 to 5 virtual users (VUs) in 5s
-    { duration: "30s", target: 5 },
+    { duration: "1m", target: 50 },
 
     // Stay at rest on 5 VUs for 10s
-    { duration: "10s", target: 5 },
+    { duration: "10s", target: 100 },
 
     // Ramp-down from 5 to 0 VUs for 5s
-    { duration: "30s", target: 0 }
+    { duration: "1m", target: 500 },    
+    
+    { duration: "2m", target: 2000 },
+
+
   ]
 }
 
 export default function () {
   let res = http.get('https://test.k6.io');
   check(res,{
-      "status is good":(r)=> r.status === 201
+      "status is good":(r)=> r.status === 200
   })
+  check(res,{
+    "staus timing":(r)=> r.timings.duration < 200
+})
   sleep(1);
 }
